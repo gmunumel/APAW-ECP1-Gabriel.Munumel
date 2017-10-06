7fb4ee2f9c24beaa1f823dd14a0daa54032923a3
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-10-06 20:00:08 +0200

    Fixed builder for Vehicle to use DriverBuilder

--- src/test/java/pd/builder/VehicleBuilderTest.java
+++ src/test/java/pd/builder/VehicleBuilderTest.java
@@ -1,6 +1,6 @@
 package pd.builder;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals; 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -16,8 +16,9 @@ public class VehicleBuilderTest {
 	@Test 
 	public void testVehicleLongStrinStringListOfDriverEnum() {
 		Vehicle vehicle = new VehicleBuilder().id(1L).brand("Ferrari").model("Carrera")
-				.driver(new Driver(1, "1234", 1L)).driver(new Driver(2, "2345", 2L))
-				.driver(new Driver(3, "2468", 3L)).build();
+				.driver(new DriverBuilder().id(1).reference("1234").phone(1L).build())
+				.driver(new DriverBuilder().id(2).reference("2345").phone(2L).build())
+				.driver(new DriverBuilder().id(3).reference("2468").phone(3L).build()).build();
 		assertEquals(1L, vehicle.getId(), 0);
 		assertEquals("Ferrari", vehicle.getBrand());
 		assertEquals("Carrera", vehicle.getModel());
@@ -40,8 +41,8 @@ public class VehicleBuilderTest {
 	
 	@Test
     public void testDriverContainsTrue() {
-		Driver driver1 = new Driver(1, "1234", 1L);
-		Driver driver2 = new Driver(2, "2468", 2L);
+		Driver driver1 = new DriverBuilder().id(1).reference("1234").phone(1L).build();
+		Driver driver2 = new DriverBuilder().id(2).reference("2468").phone(2L).build();
 		Vehicle vehicle = new VehicleBuilder().id(1L).brand("Ferrari")
 				.brand("Testarossa").driver(driver1).driver(driver2).build();
         assertTrue(vehicle.driverContains(driver1));
